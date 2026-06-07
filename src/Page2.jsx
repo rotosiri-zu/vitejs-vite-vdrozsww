d49e5e2e5e3860562f46ef824bbdf69f5d45411d
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-07 15:58:43 +0900

    クエリパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
   reture(
     <div>
       <h1>Page2ページです</h1>
       <link to="/page2/100">URL Parameter</link>
+      <br />
+      <link to="/page2/100?name=hogehoge">Query Parameter</link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParame } from "react-router-dom";
+import { useParame, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParame();
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+
     reture(
       <div>
         <h1>UrlParameterページです</h1>
         <p>パラメータは { id } です</p>
+        <p>クエリパラメータは { query.get("name") } です</p>
       </div>
     );
   };
